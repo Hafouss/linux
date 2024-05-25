9e3c95132db1cb8a576dc62a53ff621c221a8c50
Hafouss <133166457+Hafouss@users.noreply.github.com>
2024-05-25 16:03:54 +0100

    Add files via upload

---
+++ makefile.sh
@@ -0,0 +1,6 @@
+# Makefile pour générer le fichier README.md
+
+README.md: guessinggame.sh
+	echo "# Projet Guessing Game" > README.md
+	echo "\nDate et heure de génération: $$(date)" >> README.md
+	echo "\nNombre de lignes de code dans guessinggame.sh: $$(wc -l < guessinggame.sh)" >> README.md
